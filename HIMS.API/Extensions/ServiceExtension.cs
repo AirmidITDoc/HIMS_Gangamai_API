1344ed6d91a2a43a5e4a408c869d38f33bbaae78
Rachu29 <74956214+Rachu29@users.noreply.github.com>
2024-03-18 20:38:14 +0530

    All PahrmacyReport

--- HIMS.API/Extensions/ServiceExtension.cs
+++ HIMS.API/Extensions/ServiceExtension.cs
@@ -306,18 +306,18 @@ namespace HIMS.API.Extensions
             services.AddTransient<I_HomeDeliveryLogin, R_HomeDeliveryLogin>();
             services.AddTransient<I_HomeDeliveryOrder, R_HomeDeliveryOrder>();
 
-            /*  services.AddTransient<I_ProductTypeMasterHome, R_ProductTypeMasterHome>();
-           services.AddTransient<I_PayTranModeMasterHome, R_PayTranModeMasterHome>();
-           services.AddTransient<I_VendorMasterHome, R_VendorMasterHome>();
-           services.AddTransient<I_CountryMasterHome, R_CountryMasterHome>();
-           services.AddTransient<I_GenderMasterHome, R_GenderMasterHome>();
-           services.AddTransient<I_BillingClassMasterHome, R_BillingClassMasterHome>();
-           services.AddTransient<I_Itemmasterhome, R_Itemmasterhome>();
+             services.AddTransient<I_SalesReport, R_SalesReport>();
+            /*  services.AddTransient<I_PayTranModeMasterHome, R_PayTranModeMasterHome>();
+             services.AddTransient<I_VendorMasterHome, R_VendorMasterHome>();
+             services.AddTransient<I_CountryMasterHome, R_CountryMasterHome>();
+             services.AddTransient<I_GenderMasterHome, R_GenderMasterHome>();
+             services.AddTransient<I_BillingClassMasterHome, R_BillingClassMasterHome>();
+             services.AddTransient<I_Itemmasterhome, R_Itemmasterhome>();
 
-           services.AddTransient<I_ItemCategoryMasterHome, R_ItemCategoryMasterHome>();
-           services.AddTransient<I_MenuMasterHome, R_MenuMasterHome>();
-           services.AddTransient<I_MenuMasterDetailsHome, R_MenuMasterDetailsHome>();
-           services.AddTransient<I_MenuMasterDetails_DetailsHome, R_MenuMasterDetails_DetailsHome>();*/
+             services.AddTransient<I_ItemCategoryMasterHome, R_ItemCategoryMasterHome>();
+             services.AddTransient<I_MenuMasterHome, R_MenuMasterHome>();
+             services.AddTransient<I_MenuMasterDetailsHome, R_MenuMasterDetailsHome>();
+             services.AddTransient<I_MenuMasterDetails_DetailsHome, R_MenuMasterDetails_DetailsHome>();*/
 
             // services.AddTransient<>();
 
--- HIMS.Data/Pharmacy/I_Sales.cs
+++ HIMS.Data/Pharmacy/I_Sales.cs
@@ -10,37 +10,37 @@ namespace HIMS.Data.Pharmacy
         public string InsertSalesWithCredit(SalesCreditParams salesCreditParams);
         public bool PaymentSettlement(SalesParams SalesParams);
         string ViewBill(int SalesID, int OP_IP_Type,string htmlFilePath);
-        string ViewDailyCollection(DateTime FromDate, DateTime ToDate, int StoreId, int AddedById, string htmlFilePath, string HeaderName);
+        //string ViewDailyCollection(DateTime FromDate, DateTime ToDate, int StoreId, int AddedById, string htmlFilePath, string HeaderName);
 
-        string ViewDailyCollectionSummary(DateTime FromDate, DateTime ToDate, int StoreId, int AddedById, string htmlFilePath,string HeaderName);
+        //string ViewDailyCollectionSummary(DateTime FromDate, DateTime ToDate, int StoreId, int AddedById, string htmlFilePath,string HeaderName);
 
-        string ViewSalesReport(DateTime FromDate, DateTime ToDate,string SalesFromNumber, string SalesToNumber, int StoreId, int AddedBy, string htmlFilePath, string HeaderName);
-        string ViewSalesSummaryReport(DateTime FromDate, DateTime ToDate, string SalesFromNumber, string SalesToNumber, int AddedBy, int StoreId,string htmlFilePath, string Header2);
-        string ViewSalesReportPatientWise(DateTime FromDate, DateTime ToDate, string SalesFromNumber, string SalesToNumber, int AddedBy, int StoreId,string htmlFilePath, string Header2);
-        string ViewSalesReturnSummaryReport(DateTime FromDate, DateTime ToDate, string SalesFromNumber, string SalesToNumber, int StoreId,string htmlFilePath, string Header2);
+        //string ViewSalesReport(DateTime FromDate, DateTime ToDate,string SalesFromNumber, string SalesToNumber, int StoreId, int AddedBy, string htmlFilePath, string HeaderName);
+        //string ViewSalesSummaryReport(DateTime FromDate, DateTime ToDate, string SalesFromNumber, string SalesToNumber, int AddedBy, int StoreId,string htmlFilePath, string Header2);
+        //string ViewSalesReportPatientWise(DateTime FromDate, DateTime ToDate, string SalesFromNumber, string SalesToNumber, int AddedBy, int StoreId,string htmlFilePath, string Header2);
+        //string ViewSalesReturnSummaryReport(DateTime FromDate, DateTime ToDate, string SalesFromNumber, string SalesToNumber, int StoreId,string htmlFilePath, string Header2);
 
-        string ViewSalesReturnPatientwiseReport(DateTime FromDate, DateTime ToDate, string SalesFromNumber, string SalesToNumber, int StoreId, string htmlFilePath, string Header2);
-        string ViewSalesCreditReport(DateTime FromDate, DateTime ToDate, string SalesFromNumber, string SalesToNumber,int CreditReasonId, int StoreId, string htmlFilePath, string Header2);
+        //string ViewSalesReturnPatientwiseReport(DateTime FromDate, DateTime ToDate, string SalesFromNumber, string SalesToNumber, int StoreId, string htmlFilePath, string Header2);
+        //string ViewSalesCreditReport(DateTime FromDate, DateTime ToDate, string SalesFromNumber, string SalesToNumber,int CreditReasonId, int StoreId, string htmlFilePath, string Header2);
 
 
         string ViewSalesTaxReceipt(int SalesID, int OP_IP_Type, string htmlFilePath, string HeaderName);
 
-        string ViewSalesReturnReceipt(int SalesReturnID, int OP_IP_Type, string htmlFilePath, string HeaderName);
+        //string ViewSalesReturnReceipt(int SalesReturnID, int OP_IP_Type, string htmlFilePath, string HeaderName);
 
         string ViewPurchaseorderReceipt(int PurchaseID, string htmlFilePath, string HeaderName);
 
-        string ViewPharmsDailyCollectionsummaryDayandUserwise(DateTime FromDate, DateTime ToDate, int StoreId, int AddedById, string htmlFilePath, string HeaderName);
+        //string ViewPharmsDailyCollectionsummaryDayandUserwise(DateTime FromDate, DateTime ToDate, int StoreId, int AddedById, string htmlFilePath, string HeaderName);
         
         string ViewOPBillingpatientwiseReport(string PBillNo,string htmlFilePath, string Header2);
-        string ViewPharSalesCashBookReport(DateTime FromDate, DateTime ToDate, string PaymentMode, int StoreId, string htmlFilePath, string HeaderName);
+        //string ViewPharSalesCashBookReport(DateTime FromDate, DateTime ToDate, string PaymentMode, int StoreId, string htmlFilePath, string HeaderName);
 
 
-        string ViewPharOPExtcountdailyReport(DateTime FromDate, DateTime ToDate, int StoreId, string htmlFilePath, string HeaderName);
+        //string ViewPharOPExtcountdailyReport(DateTime FromDate, DateTime ToDate, int StoreId, string htmlFilePath, string HeaderName);
 
-        string ViewPharCompanycreditlistReport(int StoreId, DateTime FromDate, DateTime ToDate,string htmlFilePath, string HeaderName);
+        //string ViewPharCompanycreditlistReport(int StoreId, DateTime FromDate, DateTime ToDate,string htmlFilePath, string HeaderName);
 
 
-        string ViewPharcomwisepatientcreditReceipt(int StoreId,string htmlFilePath, string HeaderName);
+        //string ViewPharcomwisepatientcreditReceipt(int StoreId,string htmlFilePath, string HeaderName);
         string GetFilePath();
     }
 }
