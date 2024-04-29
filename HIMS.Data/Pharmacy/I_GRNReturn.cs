71c2e5a582fdfb550c4f818e0de8684903a60ae8
VIHIMS <subhash060984@gmail.com>
2024-04-29 13:08:49 +0530

    Update GRN Return Verify

--- HIMS.Data/Pharmacy/I_GRNReturn.cs
+++ HIMS.Data/Pharmacy/I_GRNReturn.cs
@@ -8,5 +8,6 @@ namespace HIMS.Data.Pharmacy
    public interface I_GRNReturn
     {
         public string InsertGRNReturn(GRNReturnParam GRNReturnParam);
+        public bool VerifyGRNReturn(GRNReturnParam GRNReturnParam);
     }
 }
