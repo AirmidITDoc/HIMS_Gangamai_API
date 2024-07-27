b481dae7cb588d4a306c5d40b9486f4f81f4771a
Rachu29 <74956214+Rachu29@users.noreply.github.com>
2024-07-27 18:33:37 +0530

    resulte

--- HIMS.Data/Pathology/I_pathresultentry.cs
+++ HIMS.Data/Pathology/I_pathresultentry.cs
@@ -7,8 +7,8 @@ namespace HIMS.Data.Pathology
 {
    public interface I_pathresultentry
     {
+       
         bool Insert(pathresultentryparam pathresultentryparam);
-
         bool PrintInsert(pathresultentryparam pathresultentryparam);
 
         string ViewPathTestReport(int OP_IP_Type, string htmlFilePath, string HeaderName);
