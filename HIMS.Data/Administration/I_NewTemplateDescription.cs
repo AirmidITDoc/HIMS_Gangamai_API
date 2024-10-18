69be96158ccd061dbea61dd5017a9a8f6e4a4920
Rachu29 <74956214+Rachu29@users.noreply.github.com>
2024-10-18 17:26:36 +0530

    New

---
+++ HIMS.Data/Administration/I_NewTemplateDescription.cs
@@ -0,0 +1,14 @@
+﻿using HIMS.Model.Administration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HIMS.Data.Administration
+{
+   public interface I_NewTemplateDescription
+    {
+        bool Insert(NewTemplateDescriptionParam NewTemplateDescriptionParam);
+
+        bool Update(NewTemplateDescriptionParam NewTemplateDescriptionParam);
+    }
+}
