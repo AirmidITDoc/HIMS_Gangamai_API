b1b2d31fd5777e85202368a02bffb1b9b5cbdc4e
Rachu29 <74956214+Rachu29@users.noreply.github.com>
2024-11-15 21:42:37 +0530

    Issuetracker

--- HIMS.Model/Inventory/IssueTrackerParams.cs
+++ HIMS.Model/Inventory/IssueTrackerParams.cs
@@ -12,29 +12,54 @@ namespace HIMS.Model.Inventory
     }
     public class InsertIssueTracker
     {
-        public DateTime IssueRaisedDate { get; set; }
-        public DateTime IssueRaisedTime { get; set; }
-        public string IssueSummary { get; set; }
+        public string IssueNo { get; set; }
+        public int ClientId { get; set; }
+        public DateTime IssueDate { get; set; }
+        public DateTime IssueTime { get; set; }
+        public string IssueName { get; set; }
         public string IssueDescription { get; set; }
-        public string UploadImagePath { get; set; }
-        public string ImageName { get; set; }
-        public string IssueStatus { get; set; }
-        public string IssueRaised { get; set; }
-        public string IssueAssigned { get; set; }
-        public long Addedby { get; set; }
+        public int RaisedById { get; set; }
+        public int AssignedToId { get; set; }
+        public int StatusId { get; set; }
+        public string DevComment { get; set; }
+        public string Comment { get; set; }
+        public int ReviewStatusId { get; set; }
+        public int ReleaseStatus { get; set; }
+        public DateTime ResolvedDate { get; set; }
+        public DateTime ResolvedTime { get; set; }
+        public string DocumentUpload { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+       
+
     }
     public class UpdateIssueTracker
     {
-        public string Operation { get; set; }
-        public long IssueTrackerId { get; set; }
-        public string IssueSummary { get; set; }
+        public long IssueId { get; set; }
+
+        public string IssueNo { get; set; }
+        public int ClientId { get; set; }
+        public DateTime IssueDate { get; set; }
+        public DateTime IssueTime { get; set; }
+        public string IssueName { get; set; }
         public string IssueDescription { get; set; }
-        public string UploadImagePath { get; set; }
-        public string ImageName { get; set; }
-        public string IssueStatus { get; set; }
-        public string IssueRaised { get; set; }
-        public string IssueAssigned { get; set; }
-        public long UpdatedBy { get; set; }
+        public int RaisedById { get; set; }
+        public int AssignedToId { get; set; }
+        public int StatusId { get; set; }
+        public string DevComment { get; set; }
+        public string Comment { get; set; }
+        public int ReviewStatusId { get; set; }
+        public int ReleaseStatus { get; set; }
+        public DateTime ResolvedDate { get; set; }
+        public DateTime ResolvedTime { get; set; }
+        public string DocumentUpload { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+
     }
     public class UpdateIssueTrackerStatus
     {
