13832bec8a4ecbd509497c332005cbde1c64e655
AirmidITDoc <130901700+AirmidITDoc@users.noreply.github.com>
2024-12-02 11:13:26 +0530

    OP Package Bill and Nursing Options

---
+++ HIMS.Model/Nursing/NursingPainAssessment.cs
@@ -0,0 +1,32 @@
+﻿using HIMS.Model.WhatsAppEmail;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HIMS.Model.Nursing
+{
+    public class NursingPainAssessment
+    {
+        public NursingPainAssessmentSave NursingPainAssessmentSave { get; set; }
+        public NursingPainAssessmentUpdate NursingPainAssessmentUpdate { get; set; }
+    }
+    public class NursingPainAssessmentSave
+    {
+        public DateTime PainAssessmentDate { get; set; }
+        public DateTime PainAssessmentTime { get; set; }
+        public long AdmissionId { get; set; }
+        public long PainAssessementValue { get; set; }
+        public long CreatedBy { get; set; }
+    }
+    public class NursingPainAssessmentUpdate
+    {
+        public long PainAssessmentId { get; set; }
+        public DateTime PainAssessmentDate { get; set; }
+        public DateTime PainAssessmentTime { get; set; }
+        public long AdmissionId { get; set; }
+        public long PainAssessementValue { get; set; }
+        public long ModifiedBy { get; set; }
+
+    }
+
+}
